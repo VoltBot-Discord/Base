768785329098d39c83b145b6412743a77abdb2da
D0wzy <52222574+D0wzy@users.noreply.github.com>
2020-09-10 16:30:08 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -9,40 +9,10 @@ module.exports = async (Sequelize, volt) => {
                 type: Sequelize.STRING(25),
                 allowNull: false
             },
-            pastUsernames: {
-                type: Sequelize.TEXT,
-                allowNull: false,
-                defaultValue: "[]"
-            },
-            pastAvatars: {
-                type: Sequelize.STRING(10000),
-                allowNull: false,
-                defaultValue: "[]"
-            },
             botBlacklisted: {
                 type: Sequelize.BOOLEAN,
                 defaultValue: false
-            },
-            afk: {
-                type: Sequelize.TEXT,
-                defaultValue: `{"enabled":false,"reason":"","date":""}`,
-                get: function () {
-                    return JSON.parse(this.getDataValue('afk'));
-                },
-                set: function (plugins) {
-                    this.setDataValue('afk', JSON.stringify(plugins));
-                },
-            },
-            reminds: {
-                type: Sequelize.TEXT,
-                defaultValue: "[]",
-                get: function() {
-                    return JSON.parse(this.getDataValue('reminds'));
-                },
-                set: function(r) {
-                    this.setDataValue('reminds', JSON.stringify(foodHabits));
-                }
-            },
+            }
         }, {
             timestamps: true
         });
