14972083acedf03f12a18b42f5a7e7885ec15759
D0wzy <52222574+D0wzy@users.noreply.github.com>
2020-09-10 16:29:47 -0400

    Update Member.js

--- models/Member.js
+++ models/Member.js
@@ -13,45 +13,22 @@ module.exports = async (Sequelize, volt) => {
                 type: Sequelize.STRING(25),
                 allowNull: false
             },
-
             xp: {
                 type: Sequelize.TEXT,
                 defaultValue: "0"
             },
-
             level: {
                 type: Sequelize.TEXT,
                 defaultValue: "0"
             },
-
             messages: {
                 type: Sequelize.INTEGER,
                 defaultValue: 0
             },
-
             guildBlacklisted: {
                 type: Sequelize.BOOLEAN,
                 defaultValue: false
-            },
-
-            sanctions: {
-                type: Sequelize.JSON,
-                defaultValue: []
-            },
-
-            mute: {
-                type: Sequelize.JSON,
-                defaultValue: {
-                    muted: false,
-                    until: Sequelize.DATE
-                }
-            },
-            banreason: {
-                type: Sequelize.STRING(255)
-            },
-            bantime: {
-                type: Sequelize.STRING(255)
-            },
+            }
         }, {
             timestamps: true
         });
